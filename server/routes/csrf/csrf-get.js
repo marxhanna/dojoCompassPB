d28c7f770aa711263664046df23a2debbbcd86cc
João Pedro de Moura Medeiros <joaomoura70718@gmail.com>
2023-09-12 10:40:10 -0300

    Mudança no código do csrf-get e csrf-post

--- server/routes/csrf/csrf-get.js
+++ server/routes/csrf/csrf-get.js
@@ -1,68 +1,51 @@
 const router = require('express').Router();
-const { errHandling, verifyLogin } = require('../../utils/utils');
+const { errHandling } = require('../../utils/utils');
 const cookieParser = require('cookie-parser');
+const csurf = require('csurf'); // Importando o modulo csurf 
 const { getUserById, updateUsername } = require('../../service/service');
 
+// Cria uma instância de token armazenado em cookie.
+const csrfProtection = csurf({ cookie: true });
+
 router.use(cookieParser());
+router.use(csrfProtection); // Aplica o middleware de proteção CSRF a todas as rotas definidas após esta linha
 
 const renderData = {};
-
-const jwt = require('jsonwebtoken');
-
 router.get(
 	'/csrf-get',
 	errHandling(async (req, res) => {
-		const { session } = req.cookies;
-		const userNotAuthenticated = session == undefined;
-		if (userNotAuthenticated) {
+		const { user_id } = req.cookies;
+		const usuarioNaoAutenticado = user_id == undefined;
+
+		if (usuarioNaoAutenticado) {
 			res.render('user-not-authenticated');
-			return;
-		} 
-		const decodedToken = verifyLogin(session);
-		if (typeof decodedToken !== 'object') {
-			renderData.error = decodedToken;
-			res.render('user-not-authenticated', renderData);
-			return;
-		} 
-		const user_id = decodedToken.id_user;
-		const { rows } = await getUserById(user_id);
-		renderData.username = rows[0].username;
-		renderData.user_id = user_id;
-		res.render('csrf-get', renderData);
+		} else {
+			const { rows } = await getUserById(user_id);
+			renderData.username = rows[0].username;
+			renderData.csrfToken = req.csrfToken() // Adicionar o token no objeto
+			res.render('csrf-get', renderData); // Renderiza a página 
+		}
 	})
 );
 
-router.get(
+// Foi alterado a propriedade router.get para router.post
+router.post( 
 	'/csrf-get/alterarusername',
 	errHandling(async (req, res) => {
-		const { id: user_id, novo_username } = req.query;
-		const { session } = req.cookies;
-		const userNotAuthenticated = session == undefined;
-		if (userNotAuthenticated) {             // VERIFICA SE EXISTE SESSÃO ATIVA
-		res.render('user-not-authenticated');
-		return;
-		}
-		const decodedToken = verifyLogin(session);
-		if (typeof decodedToken !== 'object') { // VERIFICA SE A SESSÃO É VÁLIDA
-		renderData.error = decodedToken;
-		res.render('user-not-authenticated', renderData);
-		return;
-		}
-		const id_user = decodedToken.id_user.toString();
-		// VERIFICA SE O ID PASSADO NA URL É IGUAL AO ID VINCULADO A SESSÃO
-		if (user_id !== id_user) {             
-		res.status(403).send('Proibido');
-		return;
-		}
+		//CRIA A VARIAVEI COM BASE NO QUE VEIO NA URL
+		const { novo_username } = req.query;
+		//CRIA A VARIAVEL COM BASE NO QUE ESTA NOS COOKIES
+		const { user_id } = req.cookies;
+		//BUSCA NO BANCO DE DADOS SE O USUARIO EXISTE
 		const { rows } = await getUserById(user_id);
-		const userExiste = rows.length === 1;
-		if (userExiste) {                      // VERIFICA SE USUÁRIO EXISTE
-		const { rows } = await updateUsername(novo_username, user_id);
-		renderData.username = rows[0].username;
-		res.render('csrf-get', renderData);
+		const userExiste = rows.length == 1;
+		if (userExiste) {
+			const { rows } = await updateUsername(novo_username, user_id);
+			renderData.username = rows[0].username;
+			res.render('csrf-get', renderData);
 		} else {
-		renderData.username = 'User_id_not_found';
-		res.render('csrf-get', renderData);
+			renderData.username = 'User_id_not_found';
+			res.render('csrf-get', renderData);
 		}
 	})
 );
